d26ae1afae6b2298d3279d1a20305b0c31e712a1
ThangaSaravana <btssaravana@gmail.com>
2024-10-16 23:57:24 +0530

    day 10 second problem

--- day10.js
+++ day10.js
@@ -17,3 +17,19 @@ class Solution {
          return arr1
     }
 }
+
+class Solution {
+    print(arr) {
+        // code here
+        let arr1 = []
+        arr1.push(arr[0])
+        
+        for(let i=2;i<arr.length;i++){
+            if(i%2==0){
+                arr1.push(arr[i])
+            }
+        }
+        
+        console.log(arr1.join(" "))
+    }
+}
