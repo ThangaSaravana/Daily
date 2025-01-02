1fdfff985e024fb0e698884b74caa57fd4431daa
ThangaSaravana <btssaravana@gmail.com>
2025-01-02 23:59:41 +0530

    this is for day2

--- 2025day2.js
+++ 2025day2.js
@@ -1,1 +1,19 @@
-const day2 = 2
+
+
+
+class Solution {
+    sumExceptFirstLast(arr) {
+        // code here
+        let sum = 0
+        
+        for(let i=0;i<arr.length;i++){
+            if(i==0 || i==arr.length-1){
+                sum+=0
+            }else{
+                sum+=arr[i]
+            }
+        }
+        
+        return sum
+    }
+}
