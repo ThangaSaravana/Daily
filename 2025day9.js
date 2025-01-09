b3bc405f9317f26379ba3c2e8bef00655d27655e
ThangaSaravana <134912167+ThangaSaravana@users.noreply.github.com>
2025-01-09 23:39:02 +0530

    Update 2025day9.js

--- 2025day9.js
+++ 2025day9.js
@@ -30,3 +30,41 @@ class Solution{
         return res
     }
 }
+
+///third program for the day........
+
+
+class Solution {
+    isDigitSumPalindrome(n) {
+        // code here
+        let str =  n.toString()
+        
+        let sum = 0
+        
+        for(let i = 0;i<str.length;i++){
+            
+            sum+=parseInt(str[i])
+            
+        }
+        
+        let st = sum.toString()
+        let str1 = "";
+        
+        let sumclone =  sum
+        
+        let i = 0
+        while(i<st.length){
+            let n = sumclone%10
+            str1+=n
+            let num = sumclone/10
+            sumclone = Math.floor(num)
+            i++
+        }
+        
+        if(str1==sum){
+            return true
+        }else{
+            return false
+        }
+    }
+}
