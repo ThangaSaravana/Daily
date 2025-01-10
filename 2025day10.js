2b72ae7ef2e21e910c43a2097dbcd9366ec87e7c
ThangaSaravana <134912167+ThangaSaravana@users.noreply.github.com>
2025-01-10 21:41:32 +0530

    Create 2025day10.js

---
+++ 2025day10.js
@@ -0,0 +1,32 @@
+class Solution 
+{
+    //Function to remove common characters and concatenate two strings.
+    concatenatedString(s1, s2)
+    {
+        // code here
+        let str1 = ''
+        let str2 = ""
+        
+        for(let i = 0;i<s1.length;i++){
+            if(!s2.includes(s1[i])){
+                str1+=s1[i]
+            }
+        }
+        
+        for(let i = 0;i<s2.length;i++){
+            if(!s1.includes(s2[i])){
+                str2+=s2[i]
+            }
+        }
+        
+        let outstr  = str1+str2
+        
+        if(outstr.length>0){
+            return outstr
+        }else{
+            return -1
+        }
+        
+        
+    }
+}
