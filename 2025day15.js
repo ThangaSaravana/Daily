c933886c4197eab8820d50508830e7221c69596e
ThangaSaravana <134912167+ThangaSaravana@users.noreply.github.com>
2025-01-15 18:32:07 +0530

    2nd problem for the day

--- 2025day15.js
+++ 2025day15.js
@@ -21,3 +21,28 @@ class Solution {
         return count
     }
 }
+//second  question for the day ....check if the string is plaindrome or not 
+
+
+class Solution {
+    // Function to check if a given string is a palindrome.
+    isPalindrome(s) {
+        // code here
+        if(s.length==1){
+            return true
+        }
+        
+        let nwstr = ""
+        
+        for(let i=s.length-1;i>=0;i--){
+            
+            nwstr+=s[i]
+        }
+        
+        if(nwstr === s){
+            return true
+        }else{
+            return false
+        }
+    }
+}
