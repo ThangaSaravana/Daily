1943e1a86bc10f47cb849a4773f3e69ec1204e7d
ThangaSaravana <134912167+ThangaSaravana@users.noreply.github.com>
2025-01-18 23:14:00 +0530

    first

---
+++ 2025day18.js
@@ -0,0 +1,11 @@
+class Solution {
+    simpleInterest(P, R, T) {
+        // code here
+        const ans =  (P*R*T)/100
+        
+        const final =  ans.toFixed(2)
+        return final
+    }
+}
+
+///simple interest sum .... gfg
