5af447c86fb6dd2ed12fa8efb73e06c624bb43ed
ThangaSaravana <134912167+ThangaSaravana@users.noreply.github.com>
2025-01-22 23:51:13 +0530

    odd sum and even sum

--- 2025day22.js
+++ 2025day22.js
@@ -8,3 +8,34 @@ class Solution{
         }
     }
 }
+
+/// second question a very simple question odd sum and even summm
+
+
+class Solution {
+    //Function to find sum of even and odd elements in an array
+    EvenOddSum(N, Arr)
+    {
+        //your code here
+        
+        let oddsum = 0
+        
+        let evensum = 0
+        
+        let sumarr = []
+        
+        for(let i = 0;i<Arr.length;i++){
+            
+            if((i+1)%2==0){
+                evensum+=Arr[i]
+            }else{
+                oddsum+=Arr[i]
+            }
+        }
+        
+        sumarr.push(oddsum)
+        sumarr.push(evensum)
+        
+        return sumarr
+    }
+}
