f1b309561c0fc248d996124bfec9f0cc3ca76e60
ThangaSaravana <134912167+ThangaSaravana@users.noreply.github.com>
2025-01-28 23:52:41 +0530

    c++ switch case

--- 2025day28.js
+++ 2025day28.js
@@ -19,3 +19,43 @@ class Solution {
     }
 }
 // actually this code exceeds the time complexity 
+
+// c++ switch case statement .......
+public:
+    string isInRange(int N){
+        // code here 
+        switch (N){
+            case 1:
+              return "one";
+              break;
+            case 2:
+              return "two";
+              break;
+            case 3:
+              return "three";
+              break;
+            case 4:
+              return "four";
+              break;
+            case 5:
+              return "five";
+              break;
+            case 6:
+              return "six";
+              break;
+            case 7:
+              return "seven";
+              break;
+            case 8:
+              return "eight";
+              break;
+            case 9:
+              return "nine";
+              break;
+            case 10:
+              return "ten";
+              break;
+            default:
+              return "not in range";
+        }
+    }....................
