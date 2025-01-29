ca21c11baaadb762c1bcc1bb3f03a020a2ee0d98
ThangaSaravana <134912167+ThangaSaravana@users.noreply.github.com>
2025-01-29 20:38:28 +0530

    just for sake submit

---
+++ 2025day29.js
@@ -0,0 +1,13 @@
+class Solution {
+    utility(number) {
+        // write your if statement here
+        if(number>100){
+            console.log("Big")
+            // return "Big"
+        }else{
+            console.log("Number")
+            // return "Number"
+        }
+        // write your else statement here
+    }
+}
