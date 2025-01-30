a2294747d342b15aa5993d2347a9ab4f76c037f9
ThangaSaravana <134912167+ThangaSaravana@users.noreply.github.com>
2025-01-31 00:13:33 +0530

    Minimum distance in an array

--- 2025day30.js
+++ 2025day30.js
@@ -14,3 +14,44 @@ class Solution {
     }
 }
 //.... the time complexity is O(n)
+
+// .. find the minimum distance in an array 
+
+class Solution {
+
+    minDist(arr, x, y) {
+        // code here
+       let ans = -1 
+       let xind = -1 
+       let yind = -1 
+       let min;
+       
+       for(let i = 0;i<arr.length;i++){
+           if(arr[i]==x){
+               xind = i ;
+               if(yind!=-1){
+                   if(ans==-1){
+                       ans = Math.abs(xind - yind)
+                   }else{
+                         min = Math.abs(xind - yind)
+                        ans = Math.min(ans,min)
+                   }
+               }
+           }else if(arr[i]==y){
+               yind = i;
+               if(xind != -1){
+                   if(ans==-1){
+                       ans =  Math.abs(xind - yind)
+                   }else{
+                    min =  Math.abs(xind - yind)
+                    ans = Math.min(ans,min)
+                   }
+               }
+           }
+       }
+       
+       return ans
+    }
+}                   
+
+// .... try to understand this logic .....  we are using single loop 
